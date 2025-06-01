8e7f341ea2bae0e7e2aac4c391555eec42f65825
mestash <zeuszeus1973@gmail.com>
2025-06-01 12:03:45 +0300

    -

--- backend/resources/js/bootstrap.js
+++ backend/resources/js/bootstrap.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 window.axios = axios;
+
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
 window.axios.get('/sanctum/csrf-cookie');
--- frontend/src/bootstrap.js
+++ frontend/src/bootstrap.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 window.axios = axios;
+
 window.axios.defaults.withCredentials = true;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+// Obtain the CSRF cookie required by Sanctum
 window.axios.get('/sanctum/csrf-cookie');
